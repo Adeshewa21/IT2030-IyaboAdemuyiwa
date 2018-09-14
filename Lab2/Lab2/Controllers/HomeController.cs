f2ce885e94a51e1e211bd6eefe9a9a4651ef3225
Adeshewa21 <S01096562@acad.tri-c.edu>
2018-09-14 19:20:22 -0400

    Lab2

---
+++ Lab2/Lab2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "index";
+        }
+
+        public string Browse()
+        {
+            return "Browse";
+        }
+
+        public string Details(int id)
+        {
+            return "Details";
+        }
+
+        public string Location()
+        {
+            return "zip=44124";
+        }
+    }
+}
+
---
+++ Lab2/Lab2/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product Index";
+        }
+
+        // GET: Product/Browse
+        public string Broswe()
+        {
+            return "Product Browse";
+        }
+
+        // GET: Product/Details/105
+        public string Details()
+        {
+            return "105";
+        }
+
+        // GET: Product/Locations(by genre)
+        public string Location()
+        {
+            return "zip=44124";
+        }
+    }
+}
+
