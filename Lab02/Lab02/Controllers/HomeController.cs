c8db7739227d341167335ec59edd56f25fab0da2
Adeshewa21 <S01096562@acad.tri-c.edu>
2018-09-20 19:35:22 -0400

    Lab02 and Lab03 Resubmission

---
+++ Lab02/Lab02/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About method is being called.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact method is being called.";
+
+            return View();
+        }
+
+        public string Display()
+        {
+            return "Display method is being called";
+        }
+    }
+}
---
+++ Lab02/Lab02/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+
+        // GET: Product/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        // GET: Product/Details/105 => Routing
+        public string Details(int id)
+        {
+            return "Details displayed for ID=" + id;
+        }
+
+        // GET: Product/Location (by genre) => Query string
+        public string Location(string genre)
+        {
+            return HttpUtility.HtmlEncode("Location displayed for zip=" + genre);
+        }
+    }
+}
