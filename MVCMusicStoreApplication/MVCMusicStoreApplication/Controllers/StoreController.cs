181c21ef122fd37378b8e9b5be20514219548f0b
Adeshewa21 <S01096562@acad.tri-c.edu>
2018-10-31 21:17:39 -0400

    Lab 10

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        MVCMusicStoreApplicationDB db = new MVCMusicStoreApplicationDB();
+
+        // GET: Store
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+            var list = db.Albums.ToList();
+
+            return View(list);
+        }
+
+        // GET: Store
+        [HttpGet]
+        public ActionResult Browse()
+        {
+            var list = db.Genres.ToList();
+
+            return View(list);            
+        }
+
+        // GET: Store
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var list = db.Albums.ToList();
+            return View();
+        }
+    }
+}
---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Store.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class Store
+    {
+    }
+}
